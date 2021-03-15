27a9c80c43bd9b57cdf739454991b82159b44678
OldXary <purisevicharis@gmail.com>
2021-03-15 16:18:49 +0100

    Add ex04

---
+++ ex04/src/index.js
@@ -0,0 +1,13 @@
+function localScope() {
+    var myVariable = "I am here!";
+
+
+    if (typeof myVariable != "undefined") {
+        console.log('outside localScope', myVariable)
+    } else {
+        console.log('outside localScope UNDEFINED!!!')
+    }
+}
+localScope();
+
+module.exports = localScope;
